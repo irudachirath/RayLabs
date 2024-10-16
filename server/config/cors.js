e369d42a29b5e9df4e7294204cf51223c72609fb
Iruda Chirath <iruda.21@cse.mrt.ac.lk>
2024-10-16 14:20:54 +0530

    fix/ deployment issue with vercel

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,5 +1,11 @@
 const corsConfig = {
-  origin: true,
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "https://localhost:3000",
+    "https://localhost:5000",
+    "https://ray-labs.vercel.app/",
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 };
