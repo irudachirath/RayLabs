c3021190bc367f3a698d84ad639b6977a436a19b
Vinu Kaveesha De Zoysa <vinukaveesha2001@gmail.com>
2024-10-16 15:17:59 +0530

    caching

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -6,4 +6,25 @@ firebaseAdmin.initializeApp({
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
 
+const bucket = firebaseAdmin.storage().bucket();
+
+async function setCacheControl(path) {
+  const file = bucket.file(path);
+  await file.setMetadata({
+    cacheControl: 'public, max-age=31536000'
+  });
+  console.log(`Cache-Control set for ${path}`);
+}
+
+// Paths to images in firebase
+const paths = [
+  'landing_page/logo_rayLabs.webp',
+  'landing_page/hero_image.webp',
+  'landing_page/bg_image.png',
+  'about_us_page/about_us_hero_image.webp',
+
+];
+
+paths.forEach(path => setCacheControl(path));
+
 module.exports = firebaseAdmin;
